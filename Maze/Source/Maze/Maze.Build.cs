8d746ec57008065b130aee468bc83534a647c7a9
Robert Smieja <robertsmieja@users.noreply.github.com>
2019-11-28 18:27:43 -0500

    Upgrade project description files to be 4.23 compatible

--- Maze/Source/Maze/Maze.Build.cs
+++ Maze/Source/Maze/Maze.Build.cs
@@ -4,23 +4,19 @@ using UnrealBuildTool;
 
 public class Maze : ModuleRules
 {
-	public Maze(TargetInfo Target)
+	public Maze(ReadOnlyTargetRules Target) : base(Target)
 	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
-		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
